eb21181f6dcf3048a8be6f726258b6a92a9b839f
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-12-18 03:04:11 +0900

    add literal.ts

---
+++ src/literal.ts
@@ -0,0 +1,13 @@
+export {};
+
+let dayOfTheWeek: string = '日';
+dayOfTheWeek = '木';
+dayOfTheWeek = '31';
+
+let dayOfTheWeek2: '日' | '月' | '火' | '水' | '木' | '金' | '土' = '日';
+dayOfTheWeek2 = '木';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+month = 12;
+
+let TRUE: true = true;
