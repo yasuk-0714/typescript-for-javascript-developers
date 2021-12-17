02404bad72840e7456babb0eb89d7c6f3adf35e2
Yasuhara Kenta <0714.yasuk@gmail.com>
2021-12-18 02:55:11 +0900

    add array.ts

---
+++ src/array.ts
@@ -0,0 +1,13 @@
+export {};
+
+let numbers: number[] = [1, 2, 3];
+let numbers2: Array<number> = [1, 2, 3];
+
+let strings2: string[] = ['TypeScript', 'JavaScript', 'CoffeeScript'];
+
+let nijigenhairetsu: number[][] = [
+  [50, 100],
+  [150, 300],
+];
+
+let hairetsu: (string | number | boolean)[] = [1, false, 'Japan'];
